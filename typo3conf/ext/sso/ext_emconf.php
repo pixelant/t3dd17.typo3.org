c1ed0c5485318b153abbce926a8399c5090ce99b
Mattias Nilsson <mattias@pixelant.se>
2017-01-21 20:49:47 +0100

    [TASK] Add SSO extension

---
+++ typo3conf/ext/sso/ext_emconf.php
@@ -0,0 +1,33 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'typo3.org SSO',
+    'description' => 'Single sign on for typo3.org',
+    'category' => 'fe',
+    'author' => '',
+    'author_email' => '',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => 'bottom',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 1,
+    'lockType' => '',
+    'author_company' => '',
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'cms' => '',
+            'php' => '5.5.0-0.0.0',
+            'typo3' => '7.6.0-7.99.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => '',
+);
